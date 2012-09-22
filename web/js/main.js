aa4db23a93dbefd1cf7f2b4a94f4b6e90b6a3214
Steve Lacey <steve@stevelacey.net>
2012-09-22 14:39:31 +0100

    Pulled in Sustaination data, parsed out non-local producers.

--- web/js/main.js
+++ web/js/main.js
@@ -6,17 +6,50 @@
 
     var template = Handlebars.compile($('#infowindow-template').html());
    
-    var data = [];
+    $.ajax({
+      url: 'businesses.json',
+      dataType: 'json',
+      success: function(data) {
+        var data = data.data, c, i;
+        
+        var excludes = ['bar', 'cafe', 'child', 'club', 'collectors', 'convenience', 'council', 'fast food', 'fish & chip', 'hotel', 'newsagents', 'pub', 'public house', 'restaurant', 'supermarket', 'take away', 'takeaway', 'universities'];
 
-    for (i in data) {
-      var venue = data[i];
+        for (i=0; i < data.length; i++) {
+          var supplier = data[i], skip = false;
 
-      var marker = map.addMarker({
-        title: venue.name,
-        lat: venue.location.lat,
-        lng: venue.location.lng,
-        infoWindow: {
-          content: template(venue)
+          for (c=0; c < supplier.categories.length; c++) {
+            var category = supplier.categories[c].name.toLowerCase();
+
+            for (e=0; e < excludes.length; e++) {
+              var exclude = excludes[e];
+
+              if (category.indexOf(exclude) !== -1) {
+                skip = true;
+              }
+            }
+          }
+
+          if (skip) {
+           continue;
+         } else {
+           console.log(category);
+         }
+
+          var location = supplier.location.split(',');
+          
+          supplier.location = {
+            lat: parseFloat(location[0]),
+            lng: parseFloat(location[1])
+          }
+
+          var marker = map.addMarker({
+            title: supplier.name,
+            lat: supplier.location.lat,
+            lng: supplier.location.lng,
+            infoWindow: {
+              content: template(supplier)
+            }
+          });
         }
-      });
-    }
+      }
+    });
