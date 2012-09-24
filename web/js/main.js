f0b9d69fa2ea6dd33a0943068df43ba35b9d0acf
Steve Lacey <steve@stevelacey.net>
2012-09-24 23:44:06 +0100

    Fixed indenting, refactored category parsing for custom markers.

--- web/js/main.js
+++ web/js/main.js
@@ -5,40 +5,36 @@ var map = new GMaps({
 });
 
 var template = Handlebars.compile($('#infowindow-template').html());
-   
+
 $.ajax({
   url: 'api.php',
   dataType: 'json',
   success: function(data) {
-    var c, i;
+    var c, e, i;
     var excludes = ['bar', 'cafe', 'child', 'club', 'collectors', 'convenience', 'council', 'fast food', 'fish & chip', 'hotel', 'newsagents', 'pub', 'public house', 'restaurant', 'supermarket', 'take away', 'takeaway', 'universities'];
 
     for (i=0; i < data.length; i++) {
       var supplier = data[i], skip = false;
 
-          for (e=0; e < excludes.length; e++) {
-            var exclude = excludes[e];
+      for (e=0; e < excludes.length; e++) {
+        var exclude = excludes[e];
 
-            if (supplier.name.indexOf(exclude) !== -1) {
-              skip = true;
-            }
-          }
+        if (supplier.name.indexOf(exclude) !== -1) {
+          skip = true;
+        }
+      }
 
-          if (supplier.name.indexOf('baker') !== -1) supplier.icon = 'baker';
-          if (supplier.name.indexOf('butcher') !== -1) supplier.icon = 'butcher';
-          if (supplier.name.indexOf('egg') !== -1) supplier.icon = 'eggs';
-          if (supplier.name.indexOf('fish') !== -1) supplier.icon = 'fish';
-          if (supplier.name.indexOf('grocer') !== -1) supplier.icon = 'grocery'; 
+      if (icon = categorisable(supplier.name)) {
+        supplier.icon = icon;
+      }
 
       if (supplier.categories) {
         for (c=0; c < supplier.categories.length; c++) {
           var category = supplier.categories[c].name.toLowerCase();
 
-          if (category.indexOf('baker') !== -1) supplier.icon = 'baker';
-          if (category.indexOf('butcher') !== -1) supplier.icon = 'butcher';
-          if (category.indexOf('egg') !== -1) supplier.icon = 'eggs';
-          if (category.indexOf('fish') !== -1) supplier.icon = 'fish';
-          if (category.indexOf('grocer') !== -1) supplier.icon = 'grocery';
+          if (icon = categorisable(category)) {
+            supplier.icon = icon;
+          }
 
           for (e=0; e < excludes.length; e++) {
             var exclude = excludes[e];
@@ -62,15 +58,26 @@ $.ajax({
       });
 
       if (supplier.icon) {
-                marker.setIcon(new google.maps.MarkerImage(
-                  '/images/' + supplier.icon + '.png',
-                  null,
-                  null,
-                  new google.maps.Point(16, 37),
-                  new google.maps.Size(32, 37)
-                ));
-       
+        marker.setIcon(new google.maps.MarkerImage(
+          '/images/' + supplier.icon + '.png',
+          null,
+          null,
+          new google.maps.Point(16, 37),
+          new google.maps.Size(32, 37)
+        ));
       }
     }
   }
 });
+
+function categorisable(name) {
+  var icon;
+
+  if (name.indexOf('baker') !== -1) icon = 'baker';
+  if (name.indexOf('butcher') !== -1) icon = 'butcher';
+  if (name.indexOf('egg') !== -1) icon = 'eggs';
+  if (name.indexOf('fish') !== -1) icon = 'fish';
+  if (name.indexOf('grocer') !== -1) icon = 'grocery';
+
+  return icon;
+}
